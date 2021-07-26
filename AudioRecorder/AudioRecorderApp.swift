b516677aa5cb2ac8328d53cef3b49dde5eafcd6e
yosei-yamagishi <yamagishi@radiotalk.jp>
2021-07-26 11:42:28 +0900

    UIの追加

---
+++ AudioRecorder/AudioRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderApp.swift
+//  AudioRecorder
+//
+//  Created by Yosei Yamagishi on 2021/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
